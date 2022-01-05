82740d7928dbd12062290a98e0941f7463e0b28b
John <john@adatree.com.au>
2022-01-05 15:01:01 +0000

    ADA-1073: Add placeholder react app for Storybook

---
+++ storybook/src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <h1>Placeholder app</h1>
+    <p>This Placeholder app was created in order to use Storybook to test the atomic components for the npm package.</p>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
