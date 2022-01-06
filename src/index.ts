85879cd3a8fcd3bc0cd6c7cc0440396cd99cdda1
John <john@adatree.com.au>
2022-01-06 20:29:55 +0000

    ADA-1073: Update package version to 1.07

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,8 @@ export { Test } from './atoms/test/test.atom';
 
 // Providers
 export { ThemeProvider } from './providers/theme.provider';
+
+// Utils
+export { Debugger } from './utils/debug/debug';
+export { Formatter } from './utils/formatter/formater';
+export { Logger } from './utils/logger/logger';
