c10451e6c6e8108dcab730c6d5e7ecd149fafd48
John <john@adatree.com.au>
2022-02-03 14:43:28 +0000

    ADA-1105:  Add radio button atom

---
+++ src/atoms/radio-button-with-text/radio-button-with-text.atom.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from '@mui/material';
+
+type RadioButtonItem = {
+  label: string;
+  value: string;
+  disable?: boolean;
+};
+
+export type radioButtonWithTextProps = {
+  radioButtonItems: RadioButtonItem[];
+  label?: string;
+  defaultValue?: string;
+  onChange: (value: string) => void;
+};
+export const RadioButtonWithText: React.FC<radioButtonWithTextProps> = (props) => {
+  const { label, radioButtonItems, defaultValue = '', onChange } = props;
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    onChange((event.target as HTMLInputElement).value);
+  };
+
+  return (
+    <FormControl sx={{ width: '100%' }}>
+      {label && <FormLabel id="radio-buttons-group-label">{label}</FormLabel>}
+      <RadioGroup
+        aria-labelledby="radio-buttons-group-label"
+        defaultValue={defaultValue}
+        name="radio-buttons-group"
+        onChange={handleChange}
+      >
+        {radioButtonItems.map((item) => {
+          const disabled = item.disable ? true : false;
+          return (
+            <FormControlLabel
+              sx={{ px: 1, justifyContent: 'space-between', '&:hover': { backgroundColor: 'highlight.light' } }}
+              key={item.value}
+              value={item.value}
+              control={<Radio disabled={disabled} color="secondary" />}
+              label={item.label}
+              labelPlacement="start"
+            />
+          );
+        })}
+      </RadioGroup>
+    </FormControl>
+  );
+};
---
+++ storybook/src/stories/atoms/radio-button-with-text.stories.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { RadioButtonWithText } from '../../lib';
+
+export default {
+  title: 'Atomic Components/Atoms/Radio button with text',
+  component: RadioButtonWithText,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof RadioButtonWithText>;
+
+const data = [
+  { value: 'f', label: 'Female' },
+  { value: 'm', label: 'Male' },
+  { value: 'o', label: 'Other' },
+  { value: 'd', label: 'Disabled', disable: true },
+];
+const Template: ComponentStory<typeof RadioButtonWithText> = (args) => <RadioButtonWithText {...args} />;
+
+export const DefaultButtons = Template.bind({});
+DefaultButtons.args = {
+  radioButtonItems: data,
+  onChange: (value) => {
+    alert(`The radio button with the value ${value} is selected`);
+  },
+};
+
+export const ButtonsWithValueSet = Template.bind({});
+ButtonsWithValueSet.args = {
+  radioButtonItems: data,
+  defaultValue: data[1].value,
+  onChange: (value) => {
+    alert(`The radio button with the value ${value} is selected`);
+  },
+};
+
+export const ButtonsWithLabel = Template.bind({});
+ButtonsWithLabel.args = {
+  label: 'Gender',
+  radioButtonItems: data,
+  onChange: (value) => {
+    alert(`The radio button with the value ${value} is selected`);
+  },
+};
