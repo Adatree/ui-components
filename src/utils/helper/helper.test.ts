241e6ea0c0a3576323c0f73abf3656380315207f
John <john@adatree.com.au>
2022-02-17 10:02:39 +0000

    ADA-1105:  Add dataholder filter helper

--- src/utils/helper/helper.test.ts
+++ src/utils/helper/helper.test.ts
@@ -2,7 +2,7 @@ import { Helper } from './helper';
 import { TestUtil } from '../test/test.util';
 import { Status } from '../../generated/consent';
 
-describe('List Utils', () => {
+describe('Helper Utils', () => {
   TestUtil.suspendLogger();
 
   it('should sort a list of Consents by date', () => {
@@ -39,5 +39,69 @@ describe('List Utils', () => {
     expect(revokedList.length).toEqual(2);
     expect(revokedList[0].consentId).toEqual('f328b2fb-441e-951d-2f9e-2296af82cae1');
     expect(revokedList[1].consentId).toEqual('550b2d5a-2c52-4213-a9eb-3abe467d99a9');
+  });
+
+  describe('filterDataHoldersByConsentsAndUseCase', () => {
+    it('should filter DataHolders if a consent exist with that dataholder and use case', () => {
+      const dataHolders = TestUtil.getTestDataAllDataHolders();
+      const consents = TestUtil.getTestDataConsentResponses();
+      const homeUseCase = TestUtil.getTestDataHomeUseCase();
+      const budgetingToolUseCase = TestUtil.getTestDataBudgetingToolUseCase();
+
+      const filteredByHomeUseCase = Helper.filterDataHoldersByConsentsAndUseCase(dataHolders, consents, homeUseCase);
+
+      expect(filteredByHomeUseCase.length).toEqual(1);
+      expect(filteredByHomeUseCase[0].brandName).toEqual('Yellow Bank of Australia');
+
+      const filteredByBudgetingToolUseCase = Helper.filterDataHoldersByConsentsAndUseCase(
+        dataHolders,
+        consents,
+        budgetingToolUseCase,
+      );
+
+      expect(filteredByBudgetingToolUseCase.length).toEqual(1);
+      expect(filteredByBudgetingToolUseCase[0].brandName).toEqual('Red Australia Bank');
+    });
+
+    it('should not filter DataHolders if use case is invalid', () => {
+      const dataHolders = TestUtil.getTestDataAllDataHolders();
+      const consents = TestUtil.getTestDataConsentResponses();
+      const useCase = TestUtil.getTestDataHomeUseCase();
+      useCase.id = undefined;
+
+      const filteredDataHolders01 = Helper.filterDataHoldersByConsentsAndUseCase(dataHolders, consents, useCase);
+      expect(filteredDataHolders01).toEqual(dataHolders);
+
+      const filteredDataHolders02 = Helper.filterDataHoldersByConsentsAndUseCase(dataHolders, consents, {});
+      expect(filteredDataHolders02).toEqual(dataHolders);
+    });
+
+    it('should not filter DataHolders if consents are invalid', () => {
+      const dataHolders = TestUtil.getTestDataAllDataHolders();
+      const useCase = TestUtil.getTestDataHomeUseCase();
+
+      let consent = TestUtil.getTestDataConsentResponse();
+      consent.status = undefined;
+      const filteredDataHolders01 = Helper.filterDataHoldersByConsentsAndUseCase(dataHolders, [consent], useCase);
+      expect(filteredDataHolders01).toEqual(dataHolders);
+
+      consent = TestUtil.getTestDataConsentResponse();
+      consent.dataHolderBrandId = undefined;
+      const filteredDataHolders02 = Helper.filterDataHoldersByConsentsAndUseCase(dataHolders, [consent], useCase);
+      expect(filteredDataHolders02).toEqual(dataHolders);
+
+      consent = TestUtil.getTestDataConsentResponse();
+      consent.useCase = undefined;
+      const filteredDataHolders03 = Helper.filterDataHoldersByConsentsAndUseCase(dataHolders, [consent], useCase);
+      expect(filteredDataHolders03).toEqual(dataHolders);
+
+      consent = TestUtil.getTestDataConsentResponse();
+      consent.useCase = { ...consent.useCase, id: undefined };
+      const filteredDataHolders04 = Helper.filterDataHoldersByConsentsAndUseCase(dataHolders, [consent], useCase);
+      expect(filteredDataHolders04).toEqual(dataHolders);
+
+      const filteredDataHolders05 = Helper.filterDataHoldersByConsentsAndUseCase(dataHolders, [], useCase);
+      expect(filteredDataHolders05).toEqual(dataHolders);
+    });
   });
 });
--- src/utils/helper/helper.ts
+++ src/utils/helper/helper.ts
@@ -1,5 +1,6 @@
+import { DataHolder } from '../..';
 import { DateOption } from '../../atoms/date-button/date-button.atom';
-import { ConsentResponse, Status } from '../../generated/consent';
+import { ConsentResponse, CreateConsent, Status, UseCaseResponse } from '../../generated/consent';
 
 const sortListbyDate = (list: ConsentResponse[]): ConsentResponse[] => {
   return list.sort((a, b) => {
@@ -30,6 +31,34 @@ const filterListbyStatus = (list: ConsentResponse[], filterBy: Status): ConsentR
   return list.filter((consent: ConsentResponse) => consent.status === filterBy);
 };
 
+const filterDataHoldersByConsentsAndUseCase = (
+  dataHolders: DataHolder[],
+  consents: ConsentResponse[],
+  useCase: UseCaseResponse,
+): DataHolder[] => {
+  const filteredDataHolders = dataHolders.filter((dataHolder) => {
+    return doesConsentExistForDataHolderAndUseCase(dataHolder, consents, useCase);
+  });
+
+  return filteredDataHolders.length > 0 ? filteredDataHolders : dataHolders;
+};
+
+const doesConsentExistForDataHolderAndUseCase = (
+  dataHolder: DataHolder,
+  consents: ConsentResponse[],
+  useCase: UseCaseResponse,
+): boolean => {
+  return (
+    consents.filter((consent) => {
+      return (
+        consent.status === Status.ACTIVE &&
+        consent.useCase?.id === useCase.id &&
+        consent.dataHolderBrandId === dataHolder.dataHolderBrandId
+      );
+    }).length > 0
+  );
+};
+
 const clearDateOptions = (options: DateOption[]): DateOption[] => {
   return options.map((option) => {
     return { ...option, isSelected: false };
@@ -38,6 +67,7 @@ const clearDateOptions = (options: DateOption[]): DateOption[] => {
 
 export const Helper = {
   clearDateOptions,
+  filterDataHoldersByConsentsAndUseCase,
   filterListbyStatus,
   sortListbyDate,
 };
