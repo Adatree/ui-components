0df9b42ef7f7bec6caf3d649eb84b3030f68f299
John <john@adatree.com.au>
2022-04-26 10:28:26 +0100

    ADA-1463: Add Date format with date, time and timezone

--- src/utils/formatter/formater.ts
+++ src/utils/formatter/formater.ts
@@ -2,7 +2,8 @@ import { format, parseISO } from 'date-fns';
 import { Logger } from '../logger/logger';
 
 const DATE_FORMAT = 'dd/MM/yyyy';
-const DATE_TIME_FORMAT = 'dd/MM/yyyy HH:mm:ss xx';
+const DATE_TIME_FORMAT = 'dd/MM/yyyy HH:mm';
+const DATE_TIME_TZ_FORMAT = 'dd/MM/yyyy HH:mm:ss xx';
 /*
  * We use date-fns format that returns the
  * formatted date string in the given format.
@@ -39,7 +40,23 @@ const formatDateTime = (date: Date | string | undefined): string => {
   return '';
 };
 
+const formatDateTimeTz = (date: Date | string | undefined): string => {
+  if (date instanceof Date) {
+    return format(date, DATE_TIME_TZ_FORMAT);
+  } else if (typeof date === 'string') {
+    try {
+      return format(parseISO(date), DATE_TIME_TZ_FORMAT);
+    } catch (error) {
+      Logger.error('Unable to format date', date);
+      return '';
+    }
+  }
+  Logger.error('Unable to format date', date);
+  return '';
+};
+
 export const Formatter = {
   formatDate,
   formatDateTime,
+  formatDateTimeTz,
 };
--- src/utils/formatter/formatter.test.ts
+++ src/utils/formatter/formatter.test.ts
@@ -41,14 +41,47 @@ describe('Formatter Utils', () => {
       expect(Formatter.formatDate(date4)).toEqual(expectedValidFormat4);
     });
   });
+
   describe('Formatter.formatDateTime', () => {
+    it('should format date object into a human readable format', () => {
+      const date = new Date(1607105280278);
+      const expectedInvalidFormat = '';
+      const expectedValidFormat = '04/12/2020 18:08';
+
+      expect(Formatter.formatDateTime(undefined)).toEqual(expectedInvalidFormat);
+      expect(Formatter.formatDateTime(date)).toEqual(expectedValidFormat);
+    });
+
+    it('should format date string into a human readable format', () => {
+      const date1 = '2021-01-05T12:52:09.804Z';
+      const date2 = '2022-06-15T03:01:15.446231Z';
+      const date3 = '2020-12-23T00:00:00.000Z';
+      const date4 = '2021-01-30T04:19:00Z';
+
+      const expectedInvalidFormat = '';
+      const expectedValidFormat1 = '05/01/2021 12:52';
+      const expectedValidFormat2 = '15/06/2022 04:01';
+      const expectedValidFormat3 = '23/12/2020 00:00';
+      const expectedValidFormat4 = '30/01/2021 04:19';
+
+      expect(Formatter.formatDateTime('')).toEqual(expectedInvalidFormat);
+      expect(Formatter.formatDateTime('invalid')).toEqual(expectedInvalidFormat);
+      expect(Formatter.formatDateTime(undefined)).toEqual(expectedInvalidFormat);
+      expect(Formatter.formatDateTime(date1)).toEqual(expectedValidFormat1);
+      expect(Formatter.formatDateTime(date2)).toEqual(expectedValidFormat2);
+      expect(Formatter.formatDateTime(date3)).toEqual(expectedValidFormat3);
+      expect(Formatter.formatDateTime(date4)).toEqual(expectedValidFormat4);
+    });
+  });
+
+  describe('Formatter.formatDateTimeTz', () => {
     it('should format date object into a human readable format', () => {
       const date = new Date(1607105280278);
       const expectedInvalidFormat = '';
       const expectedValidFormat = '04/12/2020 18:08:00 +0000';
 
-      expect(Formatter.formatDateTime(undefined)).toEqual(expectedInvalidFormat);
-      expect(Formatter.formatDateTime(date)).toEqual(expectedValidFormat);
+      expect(Formatter.formatDateTimeTz(undefined)).toEqual(expectedInvalidFormat);
+      expect(Formatter.formatDateTimeTz(date)).toEqual(expectedValidFormat);
     });
 
     it('should format date string into a human readable format', () => {
@@ -63,13 +96,13 @@ describe('Formatter Utils', () => {
       const expectedValidFormat3 = '23/12/2020 00:00:00 +0000';
       const expectedValidFormat4 = '30/01/2021 04:19:00 +0000';
 
-      expect(Formatter.formatDateTime('')).toEqual(expectedInvalidFormat);
-      expect(Formatter.formatDateTime('invalid')).toEqual(expectedInvalidFormat);
-      expect(Formatter.formatDateTime(undefined)).toEqual(expectedInvalidFormat);
-      expect(Formatter.formatDateTime(date1)).toEqual(expectedValidFormat1);
-      expect(Formatter.formatDateTime(date2)).toEqual(expectedValidFormat2);
-      expect(Formatter.formatDateTime(date3)).toEqual(expectedValidFormat3);
-      expect(Formatter.formatDateTime(date4)).toEqual(expectedValidFormat4);
+      expect(Formatter.formatDateTimeTz('')).toEqual(expectedInvalidFormat);
+      expect(Formatter.formatDateTimeTz('invalid')).toEqual(expectedInvalidFormat);
+      expect(Formatter.formatDateTimeTz(undefined)).toEqual(expectedInvalidFormat);
+      expect(Formatter.formatDateTimeTz(date1)).toEqual(expectedValidFormat1);
+      expect(Formatter.formatDateTimeTz(date2)).toEqual(expectedValidFormat2);
+      expect(Formatter.formatDateTimeTz(date3)).toEqual(expectedValidFormat3);
+      expect(Formatter.formatDateTimeTz(date4)).toEqual(expectedValidFormat4);
     });
   });
 });
