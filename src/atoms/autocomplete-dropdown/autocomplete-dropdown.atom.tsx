a62fe121d052e3eb4d18ba8094086e5735f90830
John <john@adatree.com.au>
2022-07-07 13:50:56 +0100

    ADA-1643: Add error handle to AutocompleteDropdown input

--- src/atoms/autocomplete-dropdown/autocomplete-dropdown.atom.tsx
+++ src/atoms/autocomplete-dropdown/autocomplete-dropdown.atom.tsx
@@ -1,4 +1,4 @@
-import React, { SyntheticEvent } from 'react';
+import React, { SyntheticEvent, useState } from 'react';
 import { Autocomplete, Avatar, TextField, Typography } from '@mui/material';
 import { DataHolder } from '../../generated/consent/api';
 import Bank from 'mdi-material-ui/Bank';
@@ -13,11 +13,22 @@ export type CheckboxAccordionProps = {
 
 export const AutocompleteDropdown: React.FC<CheckboxAccordionProps> = (props) => {
   const { dataHolders, label = '', defaultValue = null, disableDataHolders = [], onChange } = props;
+  const [inputValue, setInputValue] = useState<DataHolder | null>(null);
+  const [isInputError, setIsInputError] = useState(false);
 
   const handleChange = (event: SyntheticEvent<Element, Event>, value: DataHolder | null) => {
     if (event) {
+      setInputValue(value);
       onChange(value);
     }
+  };
+
+  const handleFocus = () => {
+    setIsInputError(false);
+  };
+
+  const handleBlur = () => {
+    setIsInputError(inputValue === null ? true : false);
   };
 
   const isDisabled = (option: DataHolder): boolean => {
@@ -35,32 +46,42 @@ export const AutocompleteDropdown: React.FC<CheckboxAccordionProps> = (props) =>
   };
 
   return (
-    <Autocomplete
-      disablePortal
-      sx={{ backgroundColor: '#fff' }}
-      options={dataHolders}
-      getOptionLabel={(option) => option.brandName}
-      getOptionDisabled={(option) => {
-        return isDisabled(option);
-      }}
-      isOptionEqualToValue={(option, value) => option.dataHolderBrandId === value.dataHolderBrandId}
-      defaultValue={defaultValue}
-      renderOption={(props, option) => (
-        <Typography {...props} variant="body1" key={option.dataHolderBrandId} sx={{ display: 'flex' }}>
-          <Avatar
-            alt="Logo"
-            src={option.logoUri}
-            component={'span'}
-            sx={{ mr: 2, img: { height: 'auto' } }}
-            variant="square"
-          >
-            <Bank />
-          </Avatar>
-          {getLabel(option)}
+    <>
+      <Autocomplete
+        disablePortal
+        className={isInputError === true ? 'error' : ''}
+        sx={{ backgroundColor: '#fff', '&.error fieldset': { borderColor: 'error.main' } }}
+        options={dataHolders}
+        getOptionLabel={(option) => option.brandName}
+        getOptionDisabled={(option) => {
+          return isDisabled(option);
+        }}
+        isOptionEqualToValue={(option, value) => option.dataHolderBrandId === value.dataHolderBrandId}
+        defaultValue={defaultValue}
+        renderOption={(props, option) => (
+          <Typography {...props} variant="body1" key={option.dataHolderBrandId} sx={{ display: 'flex' }}>
+            <Avatar
+              alt="Logo"
+              src={option.logoUri}
+              component={'span'}
+              sx={{ mr: 2, img: { height: 'auto' } }}
+              variant="square"
+            >
+              <Bank />
+            </Avatar>
+            {getLabel(option)}
+          </Typography>
+        )}
+        renderInput={(params) => <TextField {...params} label={label} />}
+        onChange={handleChange}
+        onBlur={() => handleBlur()}
+        onFocus={() => handleFocus()}
+      />
+      {isInputError && (
+        <Typography variant="body2" color="error.main">
+          This field is required.
         </Typography>
       )}
-      renderInput={(params) => <TextField {...params} label={label} />}
-      onChange={handleChange}
-    />
+    </>
   );
 };
