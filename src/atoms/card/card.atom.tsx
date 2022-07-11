cbaf6be8c6b66c2a10d8819b7ead3765668f70ba
John <john@adatree.com.au>
2022-07-11 21:15:37 +0100

    ADA-1643: Add Card atom

---
+++ src/atoms/card/card.atom.tsx
@@ -0,0 +1,21 @@
+import React, { ReactNode } from 'react';
+import { Card as MuiCard, CardProps as MuiCardProps } from '@mui/material';
+
+interface CardProps extends MuiCardProps {
+  children: ReactNode;
+  error?: boolean;
+}
+
+export const Card: React.FC<CardProps> = (props) => {
+  const { children, error = false, sx } = props;
+
+  return (
+    <MuiCard
+      sx={{ p: 2, border: '1px Solid #fff', '&.error': { borderColor: 'error.main' }, ...sx }}
+      className={error === true ? 'error' : ''}
+      elevation={0}
+    >
+      {children}
+    </MuiCard>
+  );
+};
