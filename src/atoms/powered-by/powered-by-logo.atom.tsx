ec08600c3aec00ea9968aa42fa2a2339ba530a65
John <john@adatree.com.au>
2022-07-22 08:25:07 +0100

    ADA-2047: Update powered-by-adatree image

--- src/atoms/powered-by/powered-by-logo.atom.tsx
+++ src/atoms/powered-by/powered-by-logo.atom.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
 export const PowerByLogo: React.FC = () => {
-  return <img src="https://design.adatree.com.au/assets/images/powered-by-adatree.svg" alt="Powered by Adatree" />;
+  return (
+    <img
+      src="https://design.adatree.com.au/assets/images/powered-by-adatree.png"
+      alt="Powered by Adatree"
+      style={{ maxWidth: '300px', width: '100%' }}
+    />
+  );
 };
