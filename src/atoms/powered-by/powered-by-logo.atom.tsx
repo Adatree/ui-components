ecb993242bb1c4a9303383d7c08e3cd266ccde5c
John <john@adatree.com.au>
2022-07-22 08:44:55 +0100

    ADA-2047: Reduce powered-by-adatree logo size

--- src/atoms/powered-by/powered-by-logo.atom.tsx
+++ src/atoms/powered-by/powered-by-logo.atom.tsx
@@ -5,7 +5,7 @@ export const PowerByLogo: React.FC = () => {
     <img
       src="https://design.adatree.com.au/assets/images/powered-by-adatree.png"
       alt="Powered by Adatree"
-      style={{ maxWidth: '300px', width: '100%' }}
+      style={{ maxWidth: '200px', width: '100%' }}
     />
   );
 };
