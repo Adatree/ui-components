885f13ae2d30fccd0e3444acd127a36c6c096bdf
John <john@adatree.com.au>
2022-07-27 15:44:10 +0100

    ADA-2058: Delete old consent steps and stepper

--- src/index.ts
+++ src/index.ts
@@ -24,9 +24,6 @@ export { Tooltip } from './atoms/tooltip/tooltip.atom';
 // Molecules
 export { ConsentTabs } from './molecules/consent-tabs/consent-tabs.molecule';
 export { ConsentDetails } from './molecules/consent-details/consent-details.molecule';
-export { ConsentStepperDesktop } from './molecules/consent-stepper/desktop/consent-stepper-desktop.molecule';
-export { ConsentStepperMobile } from './molecules/consent-stepper/mobile/consent-stepper-mobile.molecule';
-export { ConsentStepperResponsive } from './molecules/consent-stepper/responsive/consent-stepper-responsive.molecule';
 export { DataHolderHeader } from './molecules/data-holder-header/data-holder-header.molecule';
 export { DateSelector } from './molecules/date-selector/date-selector.molecule';
 export { DateSummary } from './molecules/date-summary/date-summary.molecule';
@@ -40,14 +37,6 @@ export { UseCaseSummary } from './molecules/use-case-summary/use-case-summary.mo
 // Organisms
 export { CreateConsentStepV2 } from './oganisms/consent-steps/create-consent-step/create-consent-step-v2.organism';
 export { CreateConsentStepV3 } from './oganisms/consent-steps/create-consent-step/create-consent-step-v3.organism';
-export { DataAccessStep } from './oganisms/consent-steps/data-access-step/data-access-step.organism';
-export { ExtendConsentDateStep } from './oganisms/consent-steps/extend-consent-date-step/extend-consent-date-step.organism';
-export { DataHolderStep } from './oganisms/consent-steps/data-holder-step/data-holder-step.organism';
-export { InfoCdrStep } from './oganisms/consent-steps/info-cdr-step/info-cdr-step.organism';
-export { InfoHowItWorksStep } from './oganisms/consent-steps/info-how-it-works-step/info-how-it-works-step.organism';
-export { InfoExtendDateStep } from './oganisms/consent-steps/info-extend-date-step/info-extend-date-step.organism';
-export { ReviewStep } from './oganisms/consent-steps/review-step/review-step.organism';
-export { UseCaseStep } from './oganisms/consent-steps/use-case-step/use-case-step.organism';
 
 // Icons
 export { ApiCogIcon } from './atoms/icons/api-cog.icon';
