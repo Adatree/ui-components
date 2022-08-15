0ef3e244b9760ead26c452fc77b2999fd2e30c3b
John <john@adatree.com.au>
2022-08-15 10:34:42 +0100

    ADA-2196: Update dataholder place holder logo

--- src/utils/test/test-data/data-holders.ts
+++ src/utils/test/test-data/data-holders.ts
@@ -4,7 +4,7 @@ const redBank = (): DataHolder => {
   return {
     dataHolderBrandId: 'a3e0c26a-db81-491f-bfb2-90ea2da621c8',
     brandName: 'Red Australia Bank',
-    logoUri: 'https://place-hold.it/100x100/FF3B3B?text=Logo',
+    logoUri: 'https://placehold.co/100x100/FF3B3B/000?text=logo',
   };
 };
 
@@ -12,7 +12,7 @@ const yellowBank = (): DataHolder => {
   return {
     dataHolderBrandId: '9a9cea5d-19c4-458b-ab79-c926455475d3',
     brandName: 'Yellow Bank of Australia',
-    logoUri: 'https://place-hold.it/100x100/FFFC3B?text=Logo',
+    logoUri: 'https://placehold.co/100x100/FFFC3B/000?text=logo',
   };
 };
 
