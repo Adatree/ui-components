8a0893c38287813d8baa1e89164d2416b213d44e
John <john@adatree.com.au>
2022-08-16 14:37:47 +0100

    ADA-2200: Update DataHolderTiles styles

--- src/atoms/data-holder-tiles/data-holder-tiles.atom.tsx
+++ src/atoms/data-holder-tiles/data-holder-tiles.atom.tsx
@@ -26,22 +26,23 @@ export const DataHolderTiles: React.FC<DataHolderTilesProps> = (props) => {
   };
 
   return (
-    <Card>
-      <Box sx={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center' }}>
-        {dataHolders.map((dataHolder) => {
-          const disabled = isDisabled(dataHolder);
-          return (
-            <Box
-              sx={{
-                width: { xs: '50%', md: '25%' },
-                cursor: disabled === true ? 'default' : 'pointer',
-                p: { xs: 0.5, sm: 2, md: 3 },
-                position: 'relative',
-                '&:hover': { opacity: disabled === true ? '1' : '0.3' },
-              }}
-              onClick={() => handleClick(dataHolder, disabled)}
-              key={dataHolder.dataHolderBrandId}
-            >
+    <Box sx={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center' }}>
+      {dataHolders.map((dataHolder) => {
+        const disabled = isDisabled(dataHolder);
+        return (
+          <Box
+            sx={{
+              width: { xs: '50%', md: '25%' },
+              cursor: disabled === true ? 'default' : 'pointer',
+              p: { xs: 0.5, sm: 2, md: 3 },
+              position: 'relative',
+              '&:hover': { opacity: disabled === true ? '1' : '0.3' },
+              alignSelf: 'stretch',
+            }}
+            onClick={() => handleClick(dataHolder, disabled)}
+            key={dataHolder.dataHolderBrandId}
+          >
+            <Card sx={{ height: '100%', alignItems: 'center', display: 'flex' }}>
               <img
                 src={dataHolder.logoUri}
                 alt={dataHolder.brandName}
@@ -51,30 +52,31 @@ export const DataHolderTiles: React.FC<DataHolderTilesProps> = (props) => {
                   opacity: disabled === true ? '0.3' : '1',
                 }}
               />
-              {disabled && (
-                <Chip
-                  sx={{
-                    m: { xs: 1, sm: 2, md: 3 },
-                    position: 'absolute',
-                    top: '5px',
-                    right: '5px',
-                    backgroundColor: 'success.main',
-                    color: 'common.white',
-                    fontWeight: 600,
-                    fontSize: '1.4rem',
-                    textTransform: 'lowercase',
-                    '& span:first-letter': {
-                      textTransform: 'capitalize',
-                    },
-                  }}
-                  label={'Active'}
-                  size="small"
-                />
-              )}
-            </Box>
-          );
-        })}
-      </Box>
-    </Card>
+            </Card>
+
+            {disabled && (
+              <Chip
+                sx={{
+                  m: { xs: 1, sm: 2, md: 3 },
+                  position: 'absolute',
+                  top: '5px',
+                  right: '5px',
+                  backgroundColor: 'success.main',
+                  color: 'common.white',
+                  fontWeight: 600,
+                  fontSize: '1.4rem',
+                  textTransform: 'lowercase',
+                  '& span:first-letter': {
+                    textTransform: 'capitalize',
+                  },
+                }}
+                label={'Active'}
+                size="small"
+              />
+            )}
+          </Box>
+        );
+      })}
+    </Box>
   );
 };
