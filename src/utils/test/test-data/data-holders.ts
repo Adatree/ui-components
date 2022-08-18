dbd0bc2a997a16e0114ed980fb5d397ff655f8c6
John <john@adatree.com.au>
2022-08-18 11:21:55 +0100

    ADA-2205: Upgrade bank logos

--- src/utils/test/test-data/data-holders.ts
+++ src/utils/test/test-data/data-holders.ts
@@ -4,7 +4,7 @@ const redBank = (): DataHolder => {
   return {
     dataHolderBrandId: 'a3e0c26a-db81-491f-bfb2-90ea2da621c8',
     brandName: 'Red Australia Bank',
-    logoUri: 'https://placehold.co/100x100/EE2115/000?text=Red%20Bank',
+    logoUri: 'https://design.adatree.com.au/assets/images/rab_logo.png',
   };
 };
 
@@ -12,7 +12,7 @@ const yellowBank = (): DataHolder => {
   return {
     dataHolderBrandId: '9a9cea5d-19c4-458b-ab79-c926455475d3',
     brandName: 'Yellow Bank of Australia',
-    logoUri: 'https://placehold.co/100x100/F7C63A/000?text=Yellow%20Bank',
+    logoUri: 'https://design.adatree.com.au/assets/images/yba_logo.png',
   };
 };
 
@@ -20,7 +20,7 @@ const blueBank = (): DataHolder => {
   return {
     dataHolderBrandId: '8a8cea5d-19c4-458b-ab79-c926455475d3',
     brandName: 'Royal Blue Bank of Australia',
-    logoUri: 'https://placehold.co/100x100/4169e1/000?text=Blue%20Bank',
+    logoUri: 'https://design.adatree.com.au/assets/images/rbba-logo.png',
   };
 };
 
@@ -28,7 +28,7 @@ const greenBank = (): DataHolder => {
   return {
     dataHolderBrandId: '7a7cea5d-19c4-458b-ab79-c926455475d3',
     brandName: 'Green Bank',
-    logoUri: 'https://placehold.co/100x100/7BB123/000?text=Green%20Bank',
+    logoUri: 'https://design.adatree.com.au/assets/images/gb-logo.png',
   };
 };
 
