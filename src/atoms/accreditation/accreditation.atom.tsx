8a5529b2220eb33b7048724af0f94796534aa357
John <john@adatree.com.au>
2022-10-18 15:18:18 +0100

    ADA-2500: Update Accreditation comp for dark mode

--- src/atoms/accreditation/accreditation.atom.tsx
+++ src/atoms/accreditation/accreditation.atom.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Typography, Box } from '@mui/material';
+import { Typography, Box, useTheme } from '@mui/material';
 import { LinkExternal } from '../links/link-external.atom';
 import { useCopy } from '../../context/copy.context';
 
@@ -13,21 +13,23 @@ export type AccreditationProps = {
 export const Accreditation: React.FC<AccreditationProps> = (props) => {
   const { accreditationNumber, cdrPolicyUrl, companyName, underCdrPrincipal = false } = props;
   const [copy] = useCopy();
+  const theme = useTheme();
 
   const cdrText =
     underCdrPrincipal === true
       ? `${copy.common.cdr_principal_label} ${accreditationNumber}`
       : `${copy.common.accredited_recipient_label} ${accreditationNumber}`;
 
+  const cdrLogo =
+    theme.palette.mode === 'light'
+      ? 'https://design.adatree.com.au/assets/logos/cdr-blue-logo.svg'
+      : 'https://design.adatree.com.au/assets/logos/cdr-white-logo.svg';
+
   return (
     <>
       <Box sx={{ display: 'flex' }}>
         <Box sx={{ mr: 3, width: { xs: '125px', sm: '175px' } }}>
-          <img
-            src="https://design.adatree.com.au/assets/images/consumer-data-right.png"
-            style={{ width: '100%' }}
-            alt={cdrText}
-          />
+          <img src={cdrLogo} style={{ width: '100%' }} alt={cdrText} />
         </Box>
         <Box>
           <Typography variant="body2" sx={{ display: 'block', mb: { xs: 0.1, sm: 0.5 } }}>
