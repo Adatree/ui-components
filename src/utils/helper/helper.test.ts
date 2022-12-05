8c4108bc16a767135bbe097b084e04ac60a3ee95
John <john@adatree.com.au>
2022-12-05 09:45:40 +0000

    ADA-2698: Update tests for active grantee consent

--- src/utils/helper/helper.test.ts
+++ src/utils/helper/helper.test.ts
@@ -18,7 +18,8 @@ describe('Helper Utils', () => {
       expect(sortedList[2].consentId).toEqual('6e485649-3113-468d-8067-5f18580476f6');
       expect(sortedList[3].consentId).toEqual('f328b2fb-441e-951d-2f9e-2296af82cae1');
       expect(sortedList[4].consentId).toEqual('abc8d9c3-6527-4349-a8fb-d1f7f90f225d');
-      expect(sortedList[5].consentId).toEqual('3ad2f7ce-18f4-451f-afb6-0077b339ddb4');
+      expect(sortedList[5].consentId).toEqual('abc8d9c3-6527-4349-a8fb-d1f7f90f225d');
+      expect(sortedList[6].consentId).toEqual('3ad2f7ce-18f4-451f-afb6-0077b339ddb4');
     });
   });
 
@@ -30,9 +31,10 @@ describe('Helper Utils', () => {
     const requestedList = Helper.filterListbyStatus(list, Status.REQUESTED);
     const revokedList = Helper.filterListbyStatus(list, Status.REVOKED);
 
-    expect(activeList.length).toEqual(2);
+    expect(activeList.length).toEqual(3);
     expect(activeList[0].consentId).toEqual('abc8d9c3-6527-4349-a8fb-d1f7f90f225d');
-    expect(activeList[1].consentId).toEqual('3ad2f7ce-18f4-451f-afb6-0077b339ddb4');
+    expect(activeList[1].consentId).toEqual('abc8d9c3-6527-4349-a8fb-d1f7f90f225d');
+    expect(activeList[2].consentId).toEqual('3ad2f7ce-18f4-451f-afb6-0077b339ddb4');
 
     expect(expiredList.length).toEqual(1);
     expect(expiredList[0].consentId).toEqual('6e485649-3113-468d-8067-5f18580476f6');
