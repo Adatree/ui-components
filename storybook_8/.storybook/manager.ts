32da44ac04653a88277924b7b2127ea91554ab92
John <john@adatree.com.au>
2024-04-11 14:44:54 +0100

    ADA-3972 Add Adatree theme to SB

---
+++ storybook_8/.storybook/manager.ts
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/manager-api';
+import Adatree from './theme';
+
+addons.setConfig({
+  theme: Adatree,
+});
---
+++ storybook_8/.storybook/theme.ts
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'dark',
+  brandTitle: 'Adatree',
+  brandUrl: 'https://design.adatree.com.au',
+  brandImage: '/assets/logos/adatree-logo.png',
+});
