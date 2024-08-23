291b3ed6c9380578c47a6a3ca86a0251f58eb892
John <john@adatree.com.au>
2024-08-23 12:36:21 +0100

    ADA-4311 Update insight flow style

--- src/organisms/consent-create/consent-create-insight.organism.tsx
+++ src/organisms/consent-create/consent-create-insight.organism.tsx
@@ -3,6 +3,7 @@ import { UseCaseResponse } from '../../generated/consent';
 import { Box, lighten, Typography } from '@mui/material';
 import { Highlight as HL } from '../../atoms/highlight-text/highlight-text.atom';
 import { DataRecipient } from '../../types/data-recipient.type';
+import { Card } from '../../atoms/card/card.atom';
 
 export type ConsentInsightFormProps = {
   useCase: UseCaseResponse;
@@ -17,27 +18,29 @@ export const ConsentCreateInsight = ({
   useCase,
 }: ConsentInsightFormProps) => {
   return (
-    <section>
+    <>
       <Box sx={{ mb: 3 }}>
         <Typography sx={{ mb: 1.6, textAlign: { xs: 'center', sm: 'left' } }} variant="h2">
           <HL>{useCase.name}</HL>
         </Typography>
       </Box>
 
-      <Typography sx={{ mb: 1.6, textAlign: { xs: 'center', sm: 'left' } }}>
-        <HL>{nonAdrDataRecipient.name}</HL> requires your consent to {useCase.name}.
-      </Typography>
+      <Card sx={{ mb: 3.6 }}>
+        <Typography sx={{ mb: 1.6, textAlign: { xs: 'center', sm: 'left' } }}>
+          <HL>{nonAdrDataRecipient.name}</HL> requires your consent to {useCase.name}.
+        </Typography>
 
-      {useCase.description && (
-        <Typography sx={{ mb: 1.6, textAlign: { xs: 'center', sm: 'left' } }}>{useCase.description}</Typography>
-      )}
+        {useCase.description && (
+          <Typography sx={{ mb: 1.6, textAlign: { xs: 'center', sm: 'left' } }}>{useCase.description}</Typography>
+        )}
 
-      <Typography variant="body2" sx={{ mb: 3, color: (theme) => lighten(theme.palette.text_main.main, 0.3) }}>
-        {nonAdrDataRecipient.name} is the Insight recipient. {nonAdrDataRecipient.name} has partnered with{' '}
-        {primaryDataRecipient.name} to securely access your {useCase.industries?.toString().toLocaleLowerCase()} data
-        and generate the Insight "{useCase.name}". {primaryDataRecipient.name} will only share the generated Insight
-        with {nonAdrDataRecipient.name}.
-      </Typography>
-    </section>
+        <Typography variant="body2" sx={{ color: (theme) => lighten(theme.palette.text_main.main, 0.3) }}>
+          {nonAdrDataRecipient.name} is the Insight recipient. {nonAdrDataRecipient.name} has partnered with{' '}
+          {primaryDataRecipient.name} to securely access your {useCase.industries?.toString().toLocaleLowerCase()} data
+          and generate the Insight "{useCase.name}". {primaryDataRecipient.name} will only share the generated Insight
+          with {nonAdrDataRecipient.name}.
+        </Typography>
+      </Card>
+    </>
   );
 };
